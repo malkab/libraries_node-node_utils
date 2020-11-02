c68918bb7d99c3c03171922dd15cd05888b6bdb0
Juan Pedro Pérez Alcántara <jp.perez.alcantara@gmail.com>
2020-11-03 00:01:50 +0100

    feat(reworked mini hashes generation function)

--- node/src/nodeutilshashing.ts
+++ node/src/nodeutilshashing.ts
@@ -30,37 +30,73 @@ export function encrypt(item: string, size: number = 10): string {
 
 /**
  *
- * Returns minihashes of the specified target lenght for a set of seeds.
+ * Returns as short as possible hashses without repeats for a set of strings,
+ * taking into account an optional existing set of keys.
+ *
+ * @param values
+ * Set of new values to create minihashes for.
+ *
+ * @param existingMiniHashes
+ * Set of existing mini hashes already in the set to avoid new hashes clashes.
+ *
+ * @param includeExisting
+ * A flag to include the existing mini hashes in the output. Existing minihashes
+ * will be added to the end of the new ones.
+ *
+ * @param time
+ * Optional use of a time seed in the hash.
+ *
+ * @return
+ * The array of the newly created hashes, not including the
  *
  */
-export function miniHash(
-  seeds: string[],
-  existingSeeds: string[] = [],
-  time: boolean = false
-): string[] {
+export function miniHash({
+    values,
+    existingMiniHashes = [],
+    includeExisting = false,
+    time = false
+  }: {
+    values: string[];
+    existingMiniHashes?: string[];
+    includeExisting?: boolean;
+    time?: boolean;
+}): string[] {
 
-  for (const x of seeds) {
+  // To store new seeds
+  const newMiniHashes: string[] = [];
 
+  // Chec
+  for (const x of values) {
+
+    // Get a full length hash
     const s: string = sha256(x, time);
 
+    // Boolean to check for repeats
     let existing: boolean = true;
 
+    // Initial length of the mini hash
     let l: number = 1;
 
+    // Loop to look for repeats
     while (existing) {
 
       existing = false;
 
+      // Get the mini hash of the current length
       const mini: string = s.substring(0, l);
 
-      if (existingSeeds.indexOf(mini) > -1) {
+      // Check if the mini hash already exist in the existing mini hashes
+      if (existingMiniHashes.indexOf(mini) > -1) {
 
         existing = true;
         l += 1;
 
       } else {
 
-        existingSeeds.push(mini);
+        // If not existing, add the new minihash to the output and to the
+        // existing mini hashes to avoid further crashes
+        newMiniHashes.push(mini);
+        existingMiniHashes.push(mini);
 
       }
 
@@ -68,7 +104,9 @@ export function miniHash(
 
   }
 
-  return existingSeeds;
+  // Check if the existing mini hashes must be included
+  return includeExisting ? [ ...newMiniHashes, ...existingMiniHashes ] :
+    newMiniHashes;
 
 }
 
--- node/test/00_quick_test.ts
+++ node/test/00_quick_test.ts
@@ -11,27 +11,3 @@ Quick Test
 ---------------------------
 
 `);
-
-import { NodeUtilsFiles } from "../src/index";
-
-console.log("D:",
-  NodeUtilsFiles.readCsvSync(
-    [ "/assets/csv_test.csv" ],
-    { header: true }
-  )
-);
-
-// NodeUtilsFiles.readCsv$([ "/assets/csv_test.csv" ], { header: true })
-// .subscribe(
-
-//   (o: any) => console.log("D: next", o),
-
-//   (error: Error) => console.log("D: error", error),
-
-//   () => console.log("D: completed")
-
-// )
-
-const d: any = [ { a: 34, b: 6 }, { a: 1, b: 5, n: 1 }];
-
-NodeUtilsFiles.writeCsvSync([ "/caca.csv" ], d, { delimiter: ";" });
