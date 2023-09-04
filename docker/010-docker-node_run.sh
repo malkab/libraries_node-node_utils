2a6b821067e6dd32561d46447dcd35b587528737
Malkab <jp.perez.alcantara@gmail.com>
2023-09-04 17:07:29 +0200

    wip

--- docker/010-docker-node_run.sh
+++ docker/010-docker-node_run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-mlkdcknoderun \
-  -u 1000:1000 \
-  -i node_utils_dev \
-  -w $(pwd)/../node \
-  -v $(pwd)/..:$(pwd)/.. \
-  -v $(pwd)/../node/test/assets/:/assets/ \
-  16.13.2
+docker run -ti --rm \
+    --name node_utils_dev \
+    --hostname node_utils_dev \
+    --user 1000:1000 \
+    --workdir $(pwd)/../node \
+    -v $(pwd)/..:$(pwd)/.. \
+    -v ~/.npmrc:/root/.npmrc \
+    -v ~/.npmrc:/home/node/.npmrc \
+    malkab/nodejs-dev:16.13.2
